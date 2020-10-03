3e02f841c15e4af2419f14a5d1b221879b8adce9
Lucy Tran <ltran2@macalester.edu>
2020-10-03 01:26:10 -0500

    Player class representing a player

---
+++ src/Player.java
@@ -0,0 +1,115 @@
+import java.util.ArrayList;
+
+import java.util.List;
+
+import edu.macalester.graphics.*;
+
+
+public class Player {
+    private String imagePath;
+    private List<Mark> marks;
+    private int playerNumber;
+
+    public Player(int playerNumber){
+        this.playerNumber = playerNumber;
+        marks = new ArrayList<>();
+        switch (playerNumber) {
+            case 1:
+                imagePath = "res/black x.jpg";
+                break;
+            case 2:
+                imagePath = "res/red o.png";
+                break;
+            case 3:
+                imagePath = "res/black o.png";
+                break;
+            case 4:
+                imagePath = "res/red x.png";
+                break;
+            case 5:
+                imagePath = "res/red check.png";
+                break;
+            case 6:
+                imagePath = "res/black check.png";
+                break;
+        }
+    }
+
+    public void addMark(GraphicsGroup board, BoardSquare square){
+        Mark newMark = new Mark(imagePath, square.getColumn(), square.getRow());
+        newMark.setMaxSize(square.getHeight() * 0.8);
+        newMark.setCenter(square.getCenter());
+        board.remove(square);
+        board.add(newMark.getSymbol());
+        marks.add(newMark);
+        MultiplayerGame.rectangleCount--;
+    }
+
+    public boolean checkWin(){
+        if (checkWinInRow()) return true;
+        if (checkWinInColumn()) return true;
+        if (checkWinInDiagonalUpRight()) return true;
+        if (checkWinInDiagonalUpLeft()){
+            return true;
+        }
+        return false;
+    }
+
+    private boolean checkWinInRow(){
+        for (Mark m: marks){
+            int count = 1;
+            for (int i = 1; i < WelcomeWindow.winCondition; i ++) {
+                if (isMarkInPosition(m.getColumn(), m.getRow() + i)) count += 1;
+            }
+            if (count == WelcomeWindow.winCondition) return true;
+        }
+        return false;
+    }
+
+    private boolean checkWinInColumn(){
+        for (Mark m: marks){
+            int count = 1;
+            for (int i = 1; i < WelcomeWindow.winCondition; i ++) {
+                if (isMarkInPosition(m.getColumn() + i, m.getRow())) count += 1;
+            }
+            if (count == WelcomeWindow.winCondition) return true;
+        }
+        return false;
+    }
+
+    private boolean checkWinInDiagonalUpRight(){
+        for (Mark m: marks){
+            int count = 1;
+           for (int i = 1; i < WelcomeWindow.winCondition; i ++) {
+                if (isMarkInPosition(m.getColumn() + i, m.getRow() + i)) count += 1;
+            }
+            if (count == WelcomeWindow.winCondition) return true;
+        }
+        return false;
+    }
+
+    private boolean checkWinInDiagonalUpLeft(){
+        for (Mark m: marks){
+            int count = 1;
+            for (int i = 1; i < WelcomeWindow.winCondition; i ++) {
+                if (isMarkInPosition(m.getColumn() + i, m.getRow() - i)) count += 1;
+            }
+            if (count == WelcomeWindow.winCondition) return true;
+        }
+        return false;
+    }
+
+    public String notifyWin(){
+        return "Player " + playerNumber + " wins the game!";
+    }
+
+
+    private boolean isMarkInPosition(int column, int row){
+        for (Mark m: marks){
+            if (m.getRow() == row && m.getColumn() == column){
+                return true;
+            }
+        }
+        return false;
+    }
+}
